49fa9275c40765892d62e42b4f7e2e17a0d76d16
n3gero <mrg0zp@gmail.com>
2025-04-18 00:55:06 +0900

    *

---
+++ game-configs/Hearthstone/New-HSDisconnect.ps1
@@ -0,0 +1,12 @@
+﻿New-NetFirewallRule `
+  -DisplayName "Heathstone Disconnect" `
+  -Direction Outbound `
+  -Program "E:\Program Files (x86)\Hearthstone\Hearthstone.exe" `
+  -Action Block `
+  -Profile Any `
+  -Enabled False `
+  -Protocol Any `
+  -RemoteAddress Any `
+  -LocalAddress Any `
+  -RemotePort Any `
+  -LocalPort Any
